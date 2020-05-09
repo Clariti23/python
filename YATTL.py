1afc8cdf640979a937e3a2b0543bc256d44d2776
Farmwell <g.ferowich@gmail.com>
2020-05-09 13:18:26 -0500

    python intro

--- YATTL.py
+++ YATTL.py
@@ -2,5 +2,7 @@
 # THEN LET IMAGINATION RUN RIOT.
 
 #What is Jupyter Notebook?
+# Web-based environment for running python scripts.
 
 #What is pandas Series or DataFrame?
+#a data frame is basically a table.
