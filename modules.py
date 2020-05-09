215b18301fb656ae123265bcc6193911b53a9bc8
Farmwell <g.ferowich@gmail.com>
2020-05-09 10:34:14 -0500

    importing core and pip modules

--- modules.py
+++ modules.py
@@ -1,8 +1,20 @@
 # A module is a file containing a set of functions to include in an application.
-# There are core python modules, modules you install with pip, and custom modules.
+# There are core python modules, modules you install with pip package manager (incl Django), and custom modules.
 
+# Core modules
 import datetime
+from datetime import date
+import time
+from time import time
 
-today = datetime.date.today()
+# Pip module
+from camelcase import CamelCase
 
+c = CamelCase()
+print(c.hump('hello there world'))
+
+# today = datetime.date.today()
+today = date.today()
+timestamp = time()
 print(today)
+print(timestamp)
