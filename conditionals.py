63551a7735d9229adcfab9359b61fb03427f5d1c
Farmwell <g.ferowich@gmail.com>
2020-05-09 09:12:43 -0500

    python conditionals

---
+++ conditionals.py
@@ -0,0 +1,66 @@
+# If/else condtions
+
+# x = 10
+# y = 20
+
+# Comparison operators
+#Simple if
+# if x > y:
+#     print(f'{x} is greater than {y}')
+
+# If/Else
+# if x > y:
+#     print(f'{x} is greater than {y}')
+# else:
+#     print(f'{y} is greater than {x}')
+
+# Elif
+
+# if x > y:
+#     print(f'{x} is greater than {y}')
+# elif x == y:
+#     print(f'{x} is equal to {y}')
+# else:
+#     print(f'{y} is greater than {x}')
+
+# Nested if
+# if x > 2:
+#     if x <= 10:
+#         print(f'{x} is greater than 2 and less than or equal to 10')
+
+# Logical operators
+
+# and
+# if x > 2 and x <= 10:
+#     print(f'{x} is greater than 2 and less than or equal to 10')
+
+# or
+# if x > 2 or x <= 10:
+#     print(f'{x} is greater than 2 and less than or equal to 10')
+
+# not
+# if not (x == y):
+#     print(f'{x} is not equal to {y}')
+
+# MEMBERSHIP OPERATORS - test if a sequence is present in an object
+x = 13
+y = 10
+nums = [1, 2, 3, 4, 5]
+
+# IN
+# if x in nums:
+#     print(x in nums)
+
+#NOT IN
+# if x not in nums:
+#     print(x not in nums)
+
+#IDENTITY OPERATORS
+
+# is
+if x is y:
+    print(x is y)
+
+# is not
+if x is not y:
+    print(x is not y)
