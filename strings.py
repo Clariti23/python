e23b0f3438f4a997df57b4f36f0462bcdf2428ea
Farmwell <g.ferowich@gmail.com>
2020-05-09 06:59:18 -0500

    built in string methods

--- strings.py
+++ strings.py
@@ -12,5 +12,51 @@ age = 27
 # print('My name is {name} and I am {age}'.format(name=name, age=age))
 
 # F-String (3.6+)
-print(f'Hello, my name is {name} and I am {age}')
+# print(f'Hello, my name is {name} and I am {age}')
+
 # String Methods
+
+s = 'hello world'
+
+# Capitalize a string
+# print(s.capitalize())
+
+# #Make all uppercase
+# print(s.upper())
+
+# # make all lower
+# print(s.lower())
+
+# #swap case
+# print(s.swapcase())
+
+#get length
+print(len(s))
+
+#Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = 'l'
+print(s.count(sub))
+
+# Starts with - returns a bool
+print(s.startswith('hello'))
+
+# Ends with
+print(s.endswith('d'))
+
+# Split a string into a list
+print(s.split())
+
+# Find position
+print(s.find('r'))
+
+#is all alphanumeric
+print(s.isalnum())
+
+#is all alphabetic
+print(s.isalpha())
+
+#is all numeric
+print(s.isnumeric())
