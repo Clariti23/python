8c7a970f56db37699ed15958dbd9564f98daefc8
Farmwell <g.ferowich@gmail.com>
2020-05-08 19:10:43 -0500

    first commit

---
+++ pythonCrashCourse.py
@@ -0,0 +1,20 @@
+# x = 1
+
+# print(x) /=> 1
+# x = 1  # int
+# y = 2.5  # float
+# name = 'Grant'  # str
+# is_cool = True  # bool
+
+# Multiple Assignment
+x, y, name, is_cool = (1, 2.5, 'Grant', True)
+# print(x, y, name, is_cool)
+
+
+# Basic math
+a = x + y
+
+# print(name, is_cool, a) #=> Grant True 3.5
+
+
+# print(type(a), a) # => <class 'float'> 3.5
