205a0858ca7d6ff9b50fb63f53a96bcb4e9150f0
Farmwell <g.ferowich@gmail.com>
2020-05-09 09:37:02 -0500

    modules

---
+++ modules.py
@@ -0,0 +1,8 @@
+# A module is a file containing a set of functions to include in an application.
+# There are core python modules, modules you install with pip, and custom modules.
+
+import datetime
+
+today = datetime.date.today()
+
+print(today)
