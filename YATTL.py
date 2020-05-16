a19e457ca27a3d58b23cafab04ef25461bbe6d06
Farmwell <g.ferowich@gmail.com>
2020-05-16 07:38:54 -0500

    learning outline file set to md

--- YATTL.py
+++
@@ -1,8 +0,0 @@
-# GOAL: MASTER THE RULES FIRST.
-# THEN LET IMAGINATION RUN RIOT.
-
-#What is Jupyter Notebook?
-# Web-based environment for running python scripts.
-
-#What is pandas Series or DataFrame?
-#a data frame is basically a table.
